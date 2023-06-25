f7355316baa419acbcda9a62f5e790cbde7cecd8
Jay Kumar <jay_tharwani1992@yahoo.com>
2023-06-25 17:17:06 -0300

    Update trAISformer.py

--- trAISformer.py
+++ trAISformer.py
@@ -1,26 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-# coding=utf-8
-# Copyright 2021, Duong Nguyen
-#
-# Licensed under the CECILL-C License;
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.cecill.info
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-"""Pytorch implementation of TrAISformer---A generative transformer for
-AIS trajectory prediction
 
-https://arxiv.org/abs/2109.03958
-
-"""
 import numpy as np
 from numpy import linalg
 import matplotlib.pyplot as plt
